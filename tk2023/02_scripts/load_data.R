73eeb94e383fb3c97e75a5186a59adb259e100a3
Martijn van Bloois <martijn@tabuladata.nl>
2023-12-05 20:40:27 +0000

    Correct capital letter

--- tk2023/02_scripts/load_data.R
+++ tk2023/02_scripts/load_data.R
@@ -2,7 +2,8 @@
 source("./tk2023/02_scripts/functions.R")
 source("./tk2023/02_scripts/municipality_map.R")
 
-partijen <- readxl::read_xlsx("./tk2023/01_data/overig/partijen.xlsx")
+partijen <- readxl::read_xlsx("./tk2023/01_data/overig/partijen.xlsx") %>% 
+  mutate(partij = ifelse(partij == "Christenunie", "ChristenUnie", partij))
 
 tk2023 <- fs::dir_ls("./tk2023/01_data/kieskring/") %>% 
   purrr::map(\(x) read_osv_file(x)) %>% 
