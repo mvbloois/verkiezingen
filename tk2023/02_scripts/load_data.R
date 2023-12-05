e7a9109cb02b05458c57d1d8876b910e77d1ee82
Martijn van Bloois <martijn@tabuladata.nl>
2023-12-05 12:04:39 +0000

    add scripts

--- tk2023/02_scripts/load_data.R
+++ tk2023/02_scripts/load_data.R
@@ -1,15 +1,13 @@
-library(tidyverse)
 
 source("./tk2023/02_scripts/functions.R")
 source("./tk2023/02_scripts/municipality_map.R")
 
-
-partijen <- read_xlsx("./tk2023/01_data/overig/partijen.xlsx")
+partijen <- readxl::read_xlsx("./tk2023/01_data/overig/partijen.xlsx")
 
 tk2023 <- fs::dir_ls("./tk2023/01_data/kieskring/") %>% 
-  map(\(x) read_osv_file(x)) %>% 
-  reduce(bind_rows) %>% 
-  bind_rows(
+  purrr::map(\(x) read_osv_file(x)) %>% 
+  purrr::reduce(dplyr::bind_rows) %>% 
+  dplyr::bind_rows(
     read_total_votes("./tk2023/01_data/gemeente/osv4-3_telling_tk2023_gemeente_rotterdam.csv")
   )
 
